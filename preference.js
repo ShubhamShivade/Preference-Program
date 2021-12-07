40dab786f4b917eae8f701ce8d59e85a6b72e92e
ShubhamShivade <shubhamshivade123@gmail.com>
2021-12-07 18:02:20 +0530

    Preference program

--- preference.js
+++ preference.js
@@ -1,19 +1,14 @@
-var S1=prompt("Enter Your First Preference Company Name");
-var S2=prompt("Enter Your Second Preference Company Name");
-var S3=prompt("Enter Your Third Preference Company Name");
+var S1 = prompt("Enter Your First Preference Company Name");
+var S2 = prompt("Enter Your Second Preference Company Name");
+var S3 = prompt("Enter Your Third Preference Company Name");
 
-var D1=prompt("Enter your first Selected Company Name");
-var D2=prompt("Enter your Second Selected Company Name");
+var D1 = prompt("Enter your first Selected Company Name");
+var D2 = prompt("Enter your Second Selected Company Name");
 
-if(S1==D1||S1==D2){
-    
-console.log(S1);
-}
-else if(S2==D1||S2==D2){
-    
-console.log(S2);
-}
-else if(S3==D1||S3==D2){
-    
-console.log(S3);
+if (S1 == D1 || S1 == D2) {
+    console.log(S1);
+} else if (S2 == D1 || S2 == D2) {
+    console.log(S2);
+} else if (S3 == D1 || S3 == D2) {
+    console.log(S3);
 }
